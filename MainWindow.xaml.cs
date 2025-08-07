3a930b63fcd113756bae13070ab1b95f3308a66d
pru755166@gmail.com <sc245680>
2025-08-07 16:43:44 +0800

    Style customization of the top navigation bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,30 +25,6 @@ namespace FilePreviewer
             InitializeComponent();
         }
 
-        private void FileButton_Click(object sender, RoutedEventArgs e)
-        {
-            FileMenuPopup.IsOpen = true;
-        }
-
-        private void FileButton_MouseEnter(object sender, MouseEventArgs e)
-        {
-            FileButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#D3D3D3"));
-        }
-
-        private void FileButton_MouseLeave(object sender, MouseEventArgs e)
-        {
-            FileButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1E1E1E"));
-        }
-
-        private void FileMenuPopup_MouseEnter(object sender, MouseEventArgs e)
-        {
-            // 鼠标移到Popup上不关闭
-        }
-
-        private void FileMenuPopup_MouseLeave(object sender, MouseEventArgs e)
-        {
-            FileMenuPopup.IsOpen = false;
-        }
 
         private void OpenFileButton_Click(Object sender, RoutedEventArgs e)
         {
